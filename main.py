0c21cfa5c025bf703a4d171443940514574e6b18
martin <ih4t3youall@gmail.com>
2021-01-30 18:12:01 -0300

    sadfasd

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 
 
 
+
 def load_url(url):
     driver = webdriver.Chrome('./chromedriver')
     driver.get(url)
@@ -17,7 +18,12 @@ def load_url(url):
 
     global cont_path
     cont_path = cont_path +1
-    path = 'fotos' + str(cont_path) + '/'
+    #path = 'fotos' + str(cont_path) + '/'
+    path = url.rsplit('/', 1)[1] + '/'
+    print('path: '+path)
+    global foto_number
+    foto_number = 0
+
     if not os.path.isdir(path):
         os.mkdir(path)
     for image_url in a_elements:
@@ -28,6 +34,8 @@ def get_image(image_url, path):
     # Open the url image, set stream to True, this will return the stream content.
     r = requests.get(image_url, stream=True)
     filename = image_url.split("/")[-1]
+    global foto_number
+    filename = str(foto_number)+'_'+ filename
     # Check if the image was retrieved successfully
     if r.status_code == 200:
         # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
@@ -42,9 +50,12 @@ def get_image(image_url, path):
     else:
         print('Image Couldn\'t be retreived')
 
+    foto_number = foto_number +1
+
 
 urls =[]
 cont_path = 0
+foto_number = 0
 while True:
     print('ingrese url , y go para finalizar')
     url = input()
